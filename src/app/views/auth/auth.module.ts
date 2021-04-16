61cdc1806d63d41b1e75044369a04c9dd8c8c1b2
littwa <littwa777@gmail.com>
2021-04-16 11:04:05 +0300

    refactor: add iтdent in auth.module

--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
+
 import { SharedModule } from '../../shared/shared.module';
 import { AuthPageComponent } from './auth.component'
-
-
 
 @NgModule({
   declarations: [AuthPageComponent],
