9016c1e5b8b3b2c8ec6fb4640e61b0c1f6fda8e7
littwa <littwa777@gmail.com>
2022-01-21 18:50:25 +0200

    init layout feat

--- src/app/entities/main/base-map/base-map.component.ts
+++ src/app/entities/main/base-map/base-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
+import { MapService } from 'src/app/services/map.service';
 
 @Component({
   selector: 'app-base-map',
@@ -11,112 +12,139 @@ export class BaseMapComponent implements OnInit {
   map;
   marker1;
   marker2;
+  marker3;
 
-  constructor() {
-    // mapboxgl.accessToken = environment.mapbox.accessToken;
-  }
+  constructor(private mapService: MapService) {}
 
   ngOnInit(): void {
-    this.init();
+    // this.initMap();
+    this.mapService.initMap();
   }
 
-  init(): void {
-    // mapboxgl.setRTLTextPlugin('https://cdn.maptiler.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js');
-    this.map = new mapboxgl.Map({
-      accessToken: environment.mapbox.accessToken,
-      container: 'map',
-      style: environment.map.style,
-      // style: environment.mapTiler.key,
-      center: [12.5, 55.5],
-      zoom: 8,
-    });
-
-    this.map.addControl(new mapboxgl.NavigationControl());
-
-// Create a default Marker and add it to the map.
-    this.marker1 = new mapboxgl.Marker()
-      .setDraggable(true)
-      .setLngLat([12.5, 55.5])
-      .addTo(this.map);
-
-
-// Create a default Marker, colored black, rotated 45 degrees.
-    this.marker2 = new mapboxgl.Marker({ color: 'red', rotation: 45 })
-      .setLngLat([11.5, 55.5])
-      .addTo(this.map);
-  }
+//   initMap(): void {
+//     this.map = new mapboxgl.Map({
+//       accessToken: environment.mapbox.accessToken,
+//       container: 'map',
+//       style: environment.map.style,
+//       center: [30.61623, 50.39548],
+//       zoom: 13,
+//     });
+//
+//     this.map.addControl(new mapboxgl.NavigationControl());
+//
+// // Create a default Marker and add it to the map.
+//     this.marker1 = new mapboxgl.Marker()
+//       .setDraggable(true)
+//       .setLngLat([30.61623, 50.39548])
+//       .addTo(this.map);
+//
+//
+// // Create a default Marker, colored black, rotated 45 degrees.
+//     this.marker2 = new mapboxgl.Marker({ color: 'red'})
+//       .setLngLat([30.60912, 50.45460])
+//       .addTo(this.map);
+//
+//     // this.marker2.getElement().addEventListener('click', (e) => {
+//     //   console.log(111, e);
+//     // });
+//
+//     console.log(100001, this.marker1);
+//     // this.marker3 = new mapboxgl.Marker({ color: 'red'})
+//     //   .setLngLat([30.60912, 50.45460])
+//     //   .addTo(this.map);
+//
+//     this.map.on('click', (e) => {
+//       console.log(e);
+//       // this.map.flyTo({
+//       //   center: e.features[0].geometry.coordinates
+//       // });
+// // {
+// //     lngLat: {
+// //         lng: 40.203,
+// //         lat: -74.451
+// //     },
+// //     originalEvent: {...},
+// //     point: {
+// //         x: 266,
+// //         y: 464
+// //     },
+// //      target: {...},
+// //      type: "click"
+// // }
+//     });
+//   }
 
   handle(): void {
     const lngLat = this.marker1.getLngLat();
     console.log(11111, lngLat);
-    this.central();
+    // this.central();
   }
-
-  central(): void {
-    // this.map.on('click', () => {
-// Add a GeoJSON source with 3 points.
-      this.map.addSource('points', {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: [
-            {
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: [12.5, 51.5]
-              }
-            },
-            {
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: [10.8, 52.5]
-              }
-            },
-            {
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: [11.5, 53.5]
-              }
-            }
-          ]
-        }
-      });
-// Add a circle layer
-      this.map.addLayer({
-        id: 'circle',
-        type: 'circle',
-        source: 'points',
-        paint: {
-          'circle-color': '#4264fb',
-          'circle-radius': 8,
-          'circle-stroke-width': 2,
-          'circle-stroke-color': '#ffffff'
-        }
-      });
-
-// Center the map on the coordinates of any clicked circle from the 'circle' layer.
-      this.map.on('click', 'circle', (e) => {
-        console.log(1000007, e);
-        this.map.flyTo({
-          center: e.features[0].geometry.coordinates
-        });
-      });
-
-// Change the cursor to a pointer when the it enters a feature in the 'circle' layer.
-      this.map.on('mouseenter', 'circle', () => {
-        this.map.getCanvas().style.cursor = 'pointer';
-      });
-
-// Change it back to a pointer when it leaves.
-      this.map.on('mouseleave', 'circle', () => {
-        this.map.getCanvas().style.cursor = '';
-      });
-    // });
-  }
+//
+//   central(): void {
+//     // this.map.on('click', () => {
+// // Add a GeoJSON source with 3 points.
+//       this.map.addSource('points', {
+//         type: 'geojson',
+//         data: {
+//           type: 'FeatureCollection',
+//           features: [
+//             {
+//               type: 'Feature',
+//               properties: {},
+//               geometry: {
+//                 type: 'Point',
+//                 coordinates: [30.550, 50.405 ]
+//               }
+//             },
+//             {
+//               type: 'Feature',
+//               properties: {},
+//               geometry: {
+//                 type: 'Point',
+//                 coordinates: [30.550, 50.431]
+//               }
+//             },
+//             {
+//               type: 'Feature',
+//               properties: {},
+//               geometry: {
+//                 type: 'Point',
+//                 coordinates: [30.458, 50.404 ]
+//               }
+//             }
+//           ]
+//         }
+//       });
+// // Add a circle layer
+//       this.map.addLayer({
+//         id: 'circle',
+//         type: 'circle',
+//         source: 'points',
+//         paint: {
+//           'circle-color': '#4264fb',
+//           'circle-radius': 8,
+//           'circle-stroke-width': 2,
+//           'circle-stroke-color': '#ffffff'
+//         }
+//       });
+//
+// // Center the map on the coordinates of any clicked circle from the 'circle' layer.
+//       this.map.on('click', 'circle', (e) => {
+//         console.log(1000007, e);
+//         this.map.flyTo({
+//           center: e.features[0].geometry.coordinates
+//         });
+//       });
+//
+// // Change the cursor to a pointer when the it enters a feature in the 'circle' layer.
+//       this.map.on('mouseenter', 'circle', () => {
+//         this.map.getCanvas().style.cursor = 'pointer';
+//       });
+//
+// // Change it back to a pointer when it leaves.
+//       this.map.on('mouseleave', 'circle', () => {
+//         this.map.getCanvas().style.cursor = '';
+//       });
+//     // });
+//   }
 }
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,124 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import * as mapboxgl from 'mapbox-gl';
+// import * as db from 'src/assets/db.json';
+import db from 'src/assets/db';
+
+@Injectable({ providedIn: 'root' })
+export class MapService {
+  public map;
+  public marker = null;
+  constructor() {}
+
+  private getData(): any {
+    console.log(db);
+    // const f = { ...db };
+    // console.log(f);
+  }
+
+  public initMap(): void {
+    this.map = new mapboxgl.Map({
+      accessToken: environment.mapbox.accessToken,
+      container: 'map',
+      style: environment.map.style,
+      center: [30.61623, 50.39548],
+      zoom: 13,
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+    this.getGeoJson();
+
+    this.getData();
+
+    // this.map.on('click', (e) => {
+    //   console.log(e);
+    // });
+  }
+
+  getGeoJson(): void {
+      this.map.on('load', () => {
+        this.map.addSource('places', { type: 'geojson', data: db });
+        //  'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+
+        // this.map.loadImage(
+        //   'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+        //   (error, image) => {
+        //     if (error) {
+        //       throw error;
+        //     }
+        //     console.log(1114, image);
+        //     this.map.addImage('custom-marker', image);
+        //   });
+
+
+
+        this.map.addLayer({
+          id: 'places1',
+          type: 'circle',
+          source: 'places',
+          paint: {
+            'circle-color': '#4264fb',
+            'circle-radius': 10,
+            'circle-stroke-width': 0,
+            'circle-stroke-color': '#000'
+          }
+        });
+
+        // this.map.addLayer({
+        //   id: 'places',
+        //   type: 'symbol',
+        //   source: 'places',
+        //   layout: {
+        //     'icon-image': 'custom-marker',
+        //     // 'text-field': ['get', 'title'],
+        //     // 'text-font': [
+        //     //   'Open Sans Semibold',
+        //     //   'Arial Unicode MS Bold'
+        //     // ],
+        //     // 'text-offset': [0, 1.25],
+        //     // 'text-anchor': 'top'
+        //     // 'icon-image': '{icon}',
+        //     // 'icon-allow-overlap': true
+        //   }
+        // });
+
+// Center the map on the coordinates of any clicked circle from the 'circle' layer.
+        this.map.on('click', 'places1', (e) => {
+        console.log(1000007, e, e.features[0]);
+        console.log(1000007, e, e.features[0].geometry.coordinates);
+          // this.marker.remove();
+        if (this.marker) {
+            this.marker.remove();
+          }
+        this.marker = new mapboxgl.Marker({ color: 'red' })
+            .setLngLat(e.features[0].geometry.coordinates)
+            .addTo(this.map);
+
+
+        // this.map.setPaintProperty('places1', 'circle-color', '#fa0000')
+        //   .setPaintProperty('places1', 'circle-radius', 12)
+        //   .setPaintProperty('places1', 'circle-stroke-width', 2);
+
+
+        this.map.flyTo({
+          center: e.features[0].geometry.coordinates,
+          zoom: 13,
+        });
+      });
+
+// Change the cursor to a pointer when the it enters a feature in the 'circle' layer.
+        this.map.on('mouseenter', 'places1', () => {
+        this.map.getCanvas().style.cursor = 'pointer';
+      });
+
+// Change it back to a pointer when it leaves.
+        this.map.on('mouseleave', 'places1', () => {
+        this.map.getCanvas().style.cursor = '';
+      });
+       });
+  }
+
+}
